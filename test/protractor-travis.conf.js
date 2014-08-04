86117eb67d985d1aaaef4cbefb05452a2a49d524
Tom Wambold <tom5760@gmail.com>
2014-08-04 13:04:44 -0400

    More tweaks to protractor config for Travis.

--- test/protractor-travis.conf.js
+++ test/protractor-travis.conf.js
@@ -3,34 +3,30 @@
 
 exports.config = {
   specs: ['e2e/**/*.js'],
+
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
   multiCapabilities: [{
     name: 'End-to-End Tests: Chrome 36',
     browserName: 'chrome',
     version: '36',
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
     build: process.env.TRAVIS_BUILD_NUMBER,
     tags: ['CI']
   }, {
     name: 'End-to-End Tests: Firefox',
     browserName: 'firefox',
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
     build: process.env.TRAVIS_BUILD_NUMBER,
     tags: ['CI']
   }, {
     name: 'End-to-End Tests: Safari',
     browserName: 'safari',
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
     build: process.env.TRAVIS_BUILD_NUMBER,
     tags: ['CI']
   }, {
     name: 'End-to-End Tests: Internet Explorer 11',
     browserName: 'internet explorer',
     version: '11',
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
     build: process.env.TRAVIS_BUILD_NUMBER,
     tags: ['CI']
   }]
